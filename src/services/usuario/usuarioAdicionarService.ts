c22f47986282b7e99d25519847ea4c4919c99666
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-09-04 09:06:26 -0300

    adição de bycrypt

--- src/services/usuario/usuarioAdicionarService.ts
+++ src/services/usuario/usuarioAdicionarService.ts
@@ -2,11 +2,17 @@ import { UsuarioPostPutRequestDTO } from '../../dto/usuarioDto/usuarioPostPutReq
 import { LoginJaExiste } from '../../error/LoginJaExiste';
 import { Usuario } from '../../model/Usuario';
 import { adicionarUsuarioRepository, buscarUsuarioPeloLoginRepository } from '../../repositories/usuarioRepository';
+import bcrypt from 'bcrypt';
 
 export const adicionarUsuarioService = async (usuarioPostPutRequestDTO: UsuarioPostPutRequestDTO): Promise<Usuario | null> => {
 	const result = await buscarUsuarioPeloLoginRepository(usuarioPostPutRequestDTO.login);
 	if (result === null) {
-		return await adicionarUsuarioRepository(usuarioPostPutRequestDTO);
+		const hashSenha = await bcrypt.hash(usuarioPostPutRequestDTO.senha, 10);
+		const usuarioComSenhaHash = {
+			...usuarioPostPutRequestDTO,
+			senha: hashSenha,
+		};
+		return await adicionarUsuarioRepository(usuarioComSenhaHash);
 	}
 	throw LoginJaExiste()
 };
