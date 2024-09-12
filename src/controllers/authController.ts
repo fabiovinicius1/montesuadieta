11ee2181a7e18918e5799f4c00f52713dd4715c2
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-09-12 09:39:59 -0300

    adiçao de rota para login admin

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from 'express';
 import { UsuarioLoginPostRequestDTO } from '../dto/usuarioDto/usuarioLoginPostRequestDTO';
 import { usuarioLoginService } from '../services/auth/usuarioLoginService';
+import { AdminPostPutRequestDTO } from '../dto/adminDto/AdminPostPutRequestDTO';
+import { adminLoginService } from '../services/auth/adminLoginService';
 
 const router = Router();
 
@@ -8,6 +10,16 @@ router.post('/login/usuario', async (req: Request, res: Response) => {
 	try {
 		const usuarioLoginPostRequestDTO: UsuarioLoginPostRequestDTO = req.body;
 		const result = await usuarioLoginService(usuarioLoginPostRequestDTO);
+		return res.status(201).json(result);
+	} catch (error: any) {
+		return res.status(error.statusCode).json({ message: error.message });
+	}
+});
+
+router.post('/login/admin', async (req: Request, res: Response) => {
+	try {
+		const adminPostPutRequestDTO: AdminPostPutRequestDTO = req.body;
+		const result = await adminLoginService(adminPostPutRequestDTO);
 		return res.status(201).json(result);
 	} catch (error: any) {
 		return res.status(error.statusCode).json({ message: error.message });
